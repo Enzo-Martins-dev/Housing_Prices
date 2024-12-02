2a6d0c5e86731707dd98f4e7ec6bbb08c1dfc1c4
Enzo Martins <enzomartinsdev5@gmail.com>
2024-12-02 12:41:07 -0300

    Add intervalo_confianca

--- data_treatment_null_test.py
+++ data_treatment_null_test.py
@@ -2,10 +2,12 @@ import pytest
 import pandas as pd
 
 df_housing_sem_nulo = pd.read_csv('sem_nulos.csv')
+df_sem_outlier_e_nulos = pd.read_csv('housing_no_nulls_nor_outliers.csv')
 
 def test_sem_nulos():
-    nulls = df_housing_sem_nulo.loc[(df_housing_sem_nulo['total_bedrooms'].isna() == True)]
-    assert len(nulls) == 0
+    nulls = df_housing_sem_nulo.isna().sum().all()
+    nulls_2 = df_sem_outlier_e_nulos.isna().sum().all()
+    assert nulls == 0 and nulls_2 == 0
 
 
 def rodar_testes():
